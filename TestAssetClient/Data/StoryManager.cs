4c99e1d47728c2ab0f204207aa4191b79ea40910
Michael Østerberg Jakobsen <michael.jakobsen@proimpactive.com>
2019-10-25 08:58:09 +0200

    Add project files.

---
+++ TestAssetClient/Data/StoryManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VersionOne.Assets;
+
+namespace TestAssetClient.Data
+{
+    class StoryManager
+    {
+
+        public List<Story> GetAll()
+        {
+            List<Story> storyList = new List<Story>();
+            
+            string instanceUrl = "https://www16.v1host.com/api-examples/api/asset";
+            string accessToken = "1.bndNO51GiliELZu1bbQdq3omgRI=";
+            
+            var v1 = new AssetClient(instanceUrl, accessToken);
+
+           
+            var assets = v1
+                .Query("Story")
+               .Select("Name", "Scope", "Estimate", "Description", "Number", "IsClosed")
+               .Retrieve();
+           
+            foreach (dynamic story in assets)
+            {
+                storyList.Add(new Story
+                {
+                    Href = story.Oid,
+                    Scope = story.Scope.Oid,
+                    Name = story.Name,
+                    Description = story.Description,
+                    Number = story.Number,
+                    IsClosed = Convert.ToBoolean(story.IsClosed)
+                    //Attachments
+                    //Owners
+                });
+            }
+
+            return storyList;
+        }
+    }
+}
---
+++ TestAssetClient/Views/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAssetClient.Data;
+using Xamarin.Forms;
+
+namespace TestAssetClient
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        readonly IList<Story> stories = new ObservableCollection<Story>();
+        readonly StoryManager manager = new StoryManager();
+
+        public MainPage()
+        {
+            BindingContext = stories;
+            InitializeComponent();
+
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            UpdateStoryList();
+        }
+        void OnRefreshing(object sender, EventArgs e)
+        {
+            
+            UpdateStoryList();
+            
+        }
+        void Refreshing(object sender, EventArgs e)
+        {
+            var list = (ListView)sender;
+            list.IsRefreshing = true;
+            UpdateStoryList();
+            list.IsRefreshing = false;
+        }
+        void UpdateStoryList()
+        {
+
+            var storyCollection = manager.GetAll();
+            stories.Clear();
+
+            foreach (Story story in storyCollection)
+            {
+                stories.Add(story);
+            }          
+        }
+    }
+}
